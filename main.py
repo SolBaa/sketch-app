3f47329867dda78e19d51c088854c44df9466569
Sol Battaglia <solbattaglia@gmail.com>
2022-01-05 09:30:03 -0300

    Updated left and right movement

--- main.py
+++ main.py
@@ -7,13 +7,25 @@ def move_forward():
     tim.forward(10)
 
 def turn_left():
-    tim.left(90)
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
 
 def turn_right():
-    tim.right(90)
+    tim.right(10)
+
+def move_backward():
+    tim.backward(10)
+
+def clear_screen():
+    tim.clear()
+
+
 
 screen.listen()
-screen.onkey(move_forward,"space")
-screen.onkey(turn_left,"Left")
-screen.onkey(turn_right,"Right")
+screen.onkey(move_forward,"w")
+screen.onkey(move_backward,"s")
+screen.onkey(turn_left,"a")
+screen.onkey(turn_right,"d")
+screen.onkey(clear_screen,"c")
+
 screen.exitonclick()
