84e29877b901ba97927493e65083d359d97fe235
Sol Battaglia <solbattaglia@gmail.com>
2022-01-05 09:22:54 -0300

    Added Turtle movement

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-ddsfds
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forward():
+    tim.forward(10)
+
+def turn_left():
+    tim.left(90)
+
+def turn_right():
+    tim.right(90)
+
+screen.listen()
+screen.onkey(move_forward,"space")
+screen.onkey(turn_left,"Left")
+screen.onkey(turn_right,"Right")
+screen.exitonclick()
